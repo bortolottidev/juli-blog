0679dc9ff7fd8515b123c4c3f478b6a927b3f922
ifritzord <88744@studenti.unimore.it>
2017-10-29 19:13:30 +0100

    update

--- indizio/models.py
+++ indizio/models.py
@@ -8,7 +8,7 @@ class Indizio(models.Model):
 		#null=True, default=NULL, through='Scelta')
 
 	def __str__(self):
-		return self.title
+		return self.title+" "+self.text[:20]
 
 class Scelta(models.Model):
 	# Molte scelta portano allo stesso indizio
